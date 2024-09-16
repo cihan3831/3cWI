5f816f640b31b415d462558a878addfd5ce6bcaa
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-09-16 09:24:00 +0200

    If_Bedingungen done

---
+++ src/ifbedingungen.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class ifbedingungen {
+    public static void main(String[] args) {
+        // Erstelle eine Zufallszahl zwischen 0 und 100
+        // Gib die Zufallszahl aus
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        System.out.println(randomNumber);
+
+        // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+        if (randomNumber < 20){
+            System.out.println("Mini");
+            // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+        }else if(randomNumber < 50){
+            System.out.println("Medium");
+            // Wenn die Zahl größer als 50 ist gib aus "Large"
+        }else if(randomNumber > 50) {
+            System.out.println("Large");
+        }
+    }
+}
