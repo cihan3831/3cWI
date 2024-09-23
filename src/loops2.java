76fa9a23b32504a0e8b7e352881b260538e11caf
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-09-23 09:23:50 +0200

    loops2 done

---
+++ src/loops2.java
@@ -0,0 +1,9 @@
+public class loops2 {
+    public static void main(String[] args) {
+      int sum=0;
+        for (int i = 2; i <=1000 ; i = i + 2) {
+            sum = sum + i;
+        }
+        System.out.println("Die Summe der geraden Zahlen beträgt " + sum);
+    }
+}
