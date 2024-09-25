f689f3dee4e154e5e6821df300b37e1161305b44
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-09-25 12:03:44 +0200

    whileloop done

---
+++ src/whileloop1.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class whileloop1 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        boolean isFinished= false;
+        int sum = 0;
+
+        while (!isFinished){
+
+            int randomValue = random.nextInt(10,30);
+
+            System.out.println("Nr: " + randomValue);
+
+            if (randomValue == 15 || randomValue == 25) {
+                isFinished = true;
+
+                System.out.println(randomValue);
+            } else{
+                sum = sum + randomValue;
+                System.out.println(sum);
+            }
+        }
+    }
+}
