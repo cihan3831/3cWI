2a9580b4ed62d6b006983609638d0f93462f063d
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-10-02 09:30:49 +0200

    loops1 Ergänzung done

--- src/loops1.java
+++ src/loops1.java
@@ -1,7 +1,9 @@
 public class loops1 {
     public static void main(String[] args) {
+        int sum = 0;
         for (int i = 1; i <= 100; i++) {
-            System.out.println(i);
+            sum = sum + i;
         }
+            System.out.println("Die Summe der Zahlen von 1 bis 100 ist: " + sum);
     }
 }
