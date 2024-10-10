5c78483fbc33c5ec22f460cce3a54c2f09169ec6
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-10-10 09:30:13 +0200

    end of lesson working on würfelbeispiel

--- src/würfelspiel.java
+++ src/würfelspiel.java
@@ -3,41 +3,54 @@ import java.util.Scanner;
 
 public class würfelspiel {
     public static void main(String[] args) {
-        //Erstelle ein Würfelspiel! Du spielst gegen den Computer.
-        // Wenn das Spiel startet (mit einem kleinen Menü), hat der Spieler 6 Würfe.
-        // Er spielt dabei gegen den Computer.
-        // Wenn die Augensumme höher ist als jene des Computers hat der Spieler gewonnen, ansonsten der Computer.
 
         Random random = new Random();
         int randomNumber = random.nextInt(6);
         Scanner scanner = new Scanner(System.in);
+        boolean isFinished = false;
 
-        int spielerPunkte = 0;
-        int computerPunkte = 0;
 
-        System.out.println("Willkommen im Würfelspiel");
-        System.out.println("Drücke 'Enter', um 6 mal zu Würfeln und gegen den Computer zu spielen!");
-        scanner.nextLine();
-        for (int i = 0; i < 6; i++) {
-            int spielerWurf = random.nextInt(6) + 1;
-            spielerPunkte = spielerPunkte + spielerWurf;
-            System.out.println("Du hast eine " + spielerWurf + "gewürfelt.");
-        }
-        System.out.println("Dein Gesamtergebnis beträgt: " + spielerPunkte);
+        while (!isFinished) {
+            System.out.println("Herzlich Willkommen im Würfelspiel!");
+            System.out.println("1: Würfelspiel Starten.");
+            System.out.println("2: Würfelspiel Beenden.");
+            int selection = scanner.nextInt();
 
-        System.out.println("Computer ist dran!");
-        for (int i = 0; i < 6; i++) {
-            int computerWurf = random.nextInt(6) + 1;
-            computerPunkte = computerPunkte + computerWurf;
-            System.out.println("Der Computer hat eine " + computerWurf + "gewürfelt.");
-        }
-        System.out.println("Das Gesamtergebnis vom Computer beträgt: " + computerPunkte);
+            if (selection == 1) {
+                int spielerPunkte = 0;
+                int computerPunkte = 0;
 
-        if (spielerPunkte > computerPunkte) {
-            System.out.println("Du hast Gewonnen. Glückwunsch!");
-        }
-        else if (spielerPunkte < computerPunkte) {
-            System.out.println("Der Computer hat gewonnen. Versuchen sie es noch einmal.");
+                    for (int i = 0; i < 6; i++) {
+                        System.out.println("Drücken sie Enter zum Würfeln!");
+                        scanner.nextLine();
+                        int spielerWurf = random.nextInt(6) + 1;
+                        spielerPunkte = spielerPunkte + spielerWurf;
+                        System.out.println(spielerPunkte);
+                        System.out.println("Du hast eine " + spielerWurf + " gewürfelt.");
+                    }
+                    System.out.println("Dein Gesamtergebnis beträgt: " + spielerPunkte);
+                    
+                    for (int i = 0; i < 6; i++) {
+                        int computerWurf = random.nextInt(6) + 1;
+                        computerPunkte = computerPunkte + computerWurf;
+                        System.out.println("Der Computer hat eine " + computerWurf + " gewürfelt.");
+                    }
+
+                    System.out.println("Das Gesamtergebnis vom Computer beträgt: " + computerPunkte);
+
+
+                    if (spielerPunkte > computerPunkte) {
+                        System.out.println("Du hast Gewonnen. Glückwunsch!");
+                    } else if (spielerPunkte < computerPunkte) {
+                        System.out.println("Der Computer hat gewonnen. Versuchen sie es noch einmal.");
+                    } else {
+                        System.out.println("Das Spiel geht Unentschieden aus. Leider gibt es keinen Gewinner.");
+                    }
+                }
+
+            if (selection == 2) {
+                isFinished = true;
+            }
         }
     }
 }
