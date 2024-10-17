6eb36dd632ba3b226c7b93d130644d5940ffb17f
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-10-17 09:26:30 +0200

    working on caeser end of lesson

---
+++ src/strings_cäsarverschlüsselung.java
@@ -0,0 +1,26 @@
+public class strings_cäsarverschlüsselung {
+    public static void main(String[] args) {
+        String result = encrypt("abc");
+        System.out.println(result);
+    }
+
+    public static String encrypt(String data) {
+
+        char[] letters = data.toCharArray();
+        for (int i = 0; i < letters.length; i++) {
+            letters[i] = (char) (letters[i] + 2);
+        }
+
+        return new String(letters);
+    }
+
+    public static String decrypt(String data) {
+
+        char[] letters = data.toCharArray();
+        for (int i = 0; i < letters.length; i++) {
+            letters[i] = (char) (letters[i] - 2);
+        }
+
+        return new String(letters);
+    }
+}
