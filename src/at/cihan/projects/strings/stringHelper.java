5164d583dc7c5446064aff3a80b0b3e9fde6ba68
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-10-23 09:27:45 +0200

    working on string helper end of lesson

--- src/at/cihan/projects/strings/stringHelper.java
+++ src/at/cihan/projects/strings/stringHelper.java
@@ -1,7 +1,19 @@
 package at.cihan.projects.strings;
 
+import java.util.Scanner;
+
 public class stringHelper {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean isFinished = false;
 
+        while (!isFinished) {
+            System.out.println("1. Palindrome");
+            System.out.println("2. Count letters");
+            System.out.println("3. Reverse String");
+            System.out.println("4. Print amount of letters");
+            System.out.println("5. Exit");
+            int selection = scanner.nextInt();
+        }
     }
 }
