86c9b5b67333ce2137bab417dea32c43bb0f0c6f
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-11-06 09:30:38 +0100

    started with worter raten end of lesson

---
+++ src/at/cihan/projects/strings/worter_raten.java
@@ -0,0 +1,14 @@
+package at.cihan.projects.strings;
+
+import java.util.Random;
+
+public class worter_raten {
+    public static void main(String[] args) {
+
+        String[] words = {"Thomas, Feder, schnell"};
+
+        Random random = new Random();
+
+
+    }
+}
