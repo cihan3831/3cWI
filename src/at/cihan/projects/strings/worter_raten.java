989ae6add49ddf1a3d435ad6c6dcc37437871aea
cihan3831 <cihan.sahin@student.htldornbirn.at>
2024-11-07 09:28:10 +0100

    working on worter raten end of lesson

--- src/at/cihan/projects/strings/worter_raten.java
+++ src/at/cihan/projects/strings/worter_raten.java
@@ -1,13 +1,30 @@
 package at.cihan.projects.strings;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class worter_raten {
+
+    public static Random random = new Random();
+    public static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        String[] words = {"Thomas, Feder, schnell"};
+        String[] words = {"Leo Messi"};
+        int randomNum = random.nextInt(words.length);
+        String word = words[randomNum].toLowerCase();
 
-        Random random = new Random();
+        char[] wordArr = word.toCharArray();
+        char[] guessedArr = new char[wordArr.length];
+
+        for (int i = 0; i < guessedArr.length; i++) {
+            guessedArr[i] = '*';
+        }
+
+        boolean isFinished = false;
+        System.out.println("Willkommen zum Wörter Raten-Spiel!");
+
+
 
 
     }
